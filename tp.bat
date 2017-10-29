29c32bf54b2c7d706df8d947860b3b67a1f2f39e
tkgjatqdfei <tkgjatqdfei@qq.com>
2017-10-29 15:24:59 +0800

    Create tp.bat

---
+++ tp.bat
@@ -0,0 +1,22 @@
+cd tp
+mkdir backup
+move auto_update.dll .\backup
+move DownloadServerNeedFileList.dat .\backup
+move libexpat.dll .\backup
+move media* .\backup
+move Microsoft* .\backup
+move mini_unzip_dll.dll .\backup
+move minizip.dll .\backup
+move module* .\backup
+move stream.dll .\backup
+move task_report.dll .\backup
+move TDPRepair.exe .\backup
+move ThunderFW.exe .\backup
+move ThunderLiveUD.exe .\backup
+move VodData.dll .\backup
+move XLBug* .\backup
+move XLCrypto.dll .\backup
+move XLFSIO.dll .\backup
+move XLLuaRuntime.dll .\backup
+move zlib1.dll .\backupxds
+
