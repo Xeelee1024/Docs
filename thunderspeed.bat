6f672ad1bb94b892aa700ee78a147bcdc330a036
tkgjatqdfei <tkgjatqdfei@qq.com>
2017-10-29 15:26:39 +0800

    Create thunderspeed.bat

---
+++ thunderspeed.bat
@@ -0,0 +1,50 @@
+cd Program
+rem rd /s/q Script
+rem rd /s/q xar
+rem rd /s/q XmpIcon
+mkdir backup
+move *.png .\backup
+move *.ico .\backup
+move 7z.dll .\backup
+move BaseIM.dll .\backup
+move BrowserSupport* .\backup
+move Community* .\backup
+move ConvertINI2XML.exe .\backup
+move DoctorServiceDLL.dll .\backup
+move hoc.dll .\backup
+move HostProcess.exe .\backup
+move LiveUD* .\backup
+move LiveUp* .\backup
+move main.swf .\backup
+move Microsoft* .\backup
+rem move minizip.dll .\backup
+move PerformanceServer.exe .\backup
+move PopupTip.dll .\backup
+move SendStaticsRequest.exe .\backup
+move TaskDb* .\backup
+move ThunderCmd.exe .\backup
+move ThunderExternal.exe .\backup
+move ThunderFW.exe .\backup
+move ThunderNewTask.exe .\backup
+move ThunderNewTaskShell.exe .\backup
+move ThunderPreload.exe .\backup
+move ThunderRegRepair.exe .\backup
+move ThunderShell.exe .\backup
+move ThunderUpdate.dll .\backup
+move ThunderXARRuntime.dll .\backup
+move UserDataConverter.exe .\backup
+move VideoShot.dll .\backup
+move VipServiceOffline.dll .\backup
+move WdScan.dll .\backup
+move Win7AppId.exe .\backup
+move XBrowser.exe .\backup
+move XLBugReport.exe .\backup
+move XLProtect.dll .\backup
+rem move XLServicePlatform* .\backup
+move XLStatSendApp.exe .\backup
+move XLUE?P* .\backup
+move XLVipBox.dll .\backup
+move XLWebBrowser.dll .\backup
+move XLWFPSetup.exe .\backup
+move Xmp.exe .\backup
+
