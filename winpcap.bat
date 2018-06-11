27657a9163cc818f24b8a340398afc871283f26c
tkgjatqdfei <tkgjatqdfei@qq.com>
2018-06-11 09:32:21 +0800

    Create winpcap.bat

---
+++ winpcap.bat
@@ -0,0 +1,6 @@
+copy /y .\system32\npf.sys %windir%\system32\drivers
+copy /y .\system32\packet.dll %windir%\system32
+copy /y .\system32\wpcap.dll %windir%\system32
+copy /y .\syswow64\packet.dll %windir%\syswow64
+copy /y .\syswow64\pthreadvc.dll %windir%\syswow64
+copy /y .\syswow64\wpcap.dll %windir%\syswow64
