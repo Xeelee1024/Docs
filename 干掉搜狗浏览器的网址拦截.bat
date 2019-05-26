39e08baa1c674b7810dad4db482630ad92105527
tkgjatqdfei <tkgjatqdfei@qq.com>
2019-05-26 10:14:44 +0800

    Update 干掉搜狗浏览器的网址拦截.bat

--- 干掉搜狗浏览器的网址拦截.bat
+++ 干掉搜狗浏览器的网址拦截.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 %1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
 
-echo 127.0.0.1 get.sogou.com >> %SystemRoot%\system32\drivers\etc\hosts
-echo 127.0.0.1 vinci.ie.sogou.com >> %SystemRoot%\system32\drivers\etc\hosts
+echo 127.0.0.1 get.sogou.com >> c:\windows\system32\drivers\etc\hosts
+echo 127.0.0.1 vinci.ie.sogou.com >> c:\windows\system32\drivers\etc\hosts
