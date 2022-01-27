190a97d2e821022ab4231fe1a979511af4607b43
vip123456789 <tkgjatqdfei@qq.com>
2022-01-28 00:25:16 +0800

    Update FFmpeg-M3U8-Downloader.bat

--- FFmpeg-M3U8-Downloader.bat
+++ FFmpeg-M3U8-Downloader.bat
@@ -1,15 +1,16 @@
 @ECHO OFF
 TITLE FFmpeg M3U8 Downloader
+cd /d "%~dp0"
 COLOR 0A
 
 ECHO Enter m3u8 link:
 SET /P URL=
+if /i "%url:~,1%%url:~-1,1%"=="""" set "url=%url:~1,-1%"
+
 ECHO Enter output filename:
 SET /P OUT=
 
-ECHO ----------------------------------------------------------------
-ECHO ----------------------downloading...----------------------------
-ECHO ----------------------------------------------------------------
+echo downloading...
 
 ffmpeg.exe -protocol_whitelist "concat,file,http,https,tcp,tls,crypto" -i "%URL%" -c copy "%OUT%"
 PAUSE
